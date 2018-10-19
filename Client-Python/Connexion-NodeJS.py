b218e7d328708c2ade7c599467af7b45aeca10c1
paul <jacobs.paul.pro@gmail.com>
2018-10-19 14:18:56 -0400

    francisation + nouveau formatage json

--- Client-Python/Connexion-NodeJS.py
+++ Client-Python/Connexion-NodeJS.py
@@ -2,24 +2,24 @@ import simplejson as json
 from socketIO_client_nexus import SocketIO, LoggingNamespace
 import donneeDAO
 
-def on_connect(msg):
+def lors_connection(msg):
 	print(json.loads(msg))
 
 def on_send():
     print("data sent")
 
-
-
-connexion = SocketIO('vps202433.vps.ovh.ca', 8080, LoggingNamespace)
-#connexion = SocketIO('vps202845.vps.ovh.ca', 8080, LoggingNamespace)
-connexion.on('salutation', on_connect)
+connection = SocketIO('vps202433.vps.ovh.ca', 8080, LoggingNamespace)
+#connection = SocketIO('vps202845.vps.ovh.ca', 8080, LoggingNamespace)
+connection.on('salutation', lors_connection)
 while True:
     stringMongoDB = donneeDAO.recupererValeur()
-    stringAEnvoyer = json.dumps(stringMongoDB) #transformation du string a envoyer en json
+    #stringAEnvoyer = "{"+json.dumps(stringMongoDB)+"}" #transformation du string a envoyer en json
+    stringAEnvoyer = "{"+stringMongoDB+"}" #transformation du string a envoyer en json
+
     print(stringAEnvoyer)
-    connexion.emit('aaa', stringMongoDB) #envoie du json
-    #connexion.emit('aaa', StringAEnvoyer)
+
+    connection.emit('aaa', stringMongoDB) #envoie du json
+
     print("sent")
-    connexion.wait(0.2)
 
-
+    connection.wait(0.2)
