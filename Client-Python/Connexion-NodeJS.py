b4ca202bf60f0415fbb0d711b9d98bae4f837ae0
paul <jacobs.paul.pro@gmail.com>
2018-10-23 15:30:53 -0400

    Fix (Transmission) : Json format

--- Client-Python/Connexion-NodeJS.py
+++ Client-Python/Connexion-NodeJS.py
@@ -8,13 +8,17 @@ def lors_connection(msg):
 def on_send():
     print("data sent")
 
+def lors_donnee_recu():
+    donneeDAO.supprimerDonnees()
+
 connection = SocketIO('vps202433.vps.ovh.ca', 8080, LoggingNamespace)
 #connection = SocketIO('vps202845.vps.ovh.ca', 8080, LoggingNamespace)
 connection.on('salutation', lors_connection)
+connection.on('donnee_recu',lors_donnee_recu)
 while True:
     stringMongoDB = donneeDAO.recupererValeur()
     #stringAEnvoyer = "{"+json.dumps(stringMongoDB)+"}" #transformation du string a envoyer en json
-    stringAEnvoyer = "{"+stringMongoDB+"}" #transformation du string a envoyer en json
+    stringAEnvoyer = "{"+json.dumps(stringMongoDB)+"}" #transformation du string a envoyer en json
 
     print(stringAEnvoyer)
 
--- Client-Python/donneeDAO.py
+++ Client-Python/donneeDAO.py
@@ -24,7 +24,6 @@ def recupererValeur():
         print("connected")
 
         db = conn.scientifique #selection de la base de donnee
-
         collection = db.donneeBouee #selection de la collection
 
         curseur = collection.find() #recuperation de toutes les donnees de la collection
@@ -35,10 +34,17 @@ def recupererValeur():
             resultString+=str(enregistrement) #formatage des donnees
             
         #curseur.close()
-        collection.drop() #suppression des donnees en local
             
         return str(resultString)
         
     except Exception as e:
     
         return e
+
+def supprimerDonnee():
+        conn = MongoClient()
+        print("connected")
+        db = conn.scientifique #selection de la base de donnee
+        collection = db.donneeBouee #selection de la collection
+    
+        collection.drop() #suppression des donnees en local
